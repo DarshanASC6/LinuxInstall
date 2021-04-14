3aa5a04afde7455252c0dbf1061b69cce4445cf8
DarshanASC6 <darshnyc@gmail.com>
2021-04-14 18:02:03 -0400

    updated pacman script

--- pacman-install.sh
+++ pacman-install.sh
@@ -23,4 +23,33 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 #One final update
 sudo pacman -Syu
 
-echo "Finished"
+echo "Finished installing software"
+
+#Install dwm, st and dmenu in .config folder
+
+dwm="https://git.suckless.org/dwm"
+
+st="https://git.suckless.org/st"
+
+dmenu="https://git.suckless.org/dmenu"
+
+slstatus="https://git.suckless.org/slstatus"
+
+#Clone the latest build of dwm from source
+git clone "$dwm" ".config/suckless"
+cd dwm
+sudo make clean install
+cd ..
+
+#Clone the latest build of st from source
+git clone "$st" ".config/suckless"
+cd st
+sudo make clean install
+cd ..
+
+#Clone the lates build of dmenu from the source
+git clone "$dmenu" ".config/suckless"
+cd dmenu
+sudo make clean install
+
+echo "Finished installing suckless software"
