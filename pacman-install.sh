fe689b71b21e0a0aa5cebb2b0a615de9cbc41008
darshan <darshnyc@gmail.com>
2021-04-23 12:23:29 -0400

    Changed pacman script to simply install everything, including dwm, on a pacman-based distro

--- pacman-install.sh
+++ pacman-install.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
-#Installs the following programs:
 
-#FireFox
-#VLC Media Player
-#Neofetch
-#Git
-#Vim
-#Lollypop Music Player
-#Libre Office
-#Zathura PDF Reader
-#Zathura add-ons for PDF and EPUB support
-#mpv Media Player
-#xwallpaper Wallpaper config
-#xsetroot
+echo "Installing various applications."
 
-sudo pacman -S firefox vlc neofetch git vim lollypop libreoffice zathura zathura-pdf-mupdf nitrogen xorg-xsetroot
+# List of programs installed:
+# Firefox
+# VLC Media Player
+# Neofetch
+# Git
+# Vim
+# Lollypop Music Player
+# Libre Office
+# Zathura PDF Reader
+# Zathura add-ons for PDF and EPUB support
+# mpv Media Player
+# nitrogen
+# xsetroot
+sudo pacman -S firefox vlc neofetch git vim lollypop libreoffice zathura zathura-pdf-mupdf mpv nitrogen xorg-xsetroot
 
 #Set up vim plug installer
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -23,9 +24,11 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 #One final update
 sudo pacman -Syu
 
-echo "Finished installing software"
+echo "Finished installing applications."
 
-#Install dwm, st and dmenu in .config folder
+# Install dwm, st and dmenu in .config folder
+
+echo "Installing desktop enviornment."
 
 dwm="https://git.suckless.org/dwm"
 
@@ -35,21 +38,72 @@ dmenu="https://git.suckless.org/dmenu"
 
 slstatus="https://git.suckless.org/slstatus"
 
-#Clone the latest build of dwm from source
-git clone "$dwm" ".config/suckless"
+mkdir $HOME/.config/suckless
+
+cd $HOME/.config/suckless
+
+# The below clone sections could probably be replaced with a function.
+
+# Clone the latest build of dwm from source
+git clone "$dwm"
 cd dwm
 sudo make clean install
 cd ..
 
 #Clone the latest build of st from source
-git clone "$st" ".config/suckless"
+git clone "$st"
 cd st
 sudo make clean install
 cd ..
 
 #Clone the lates build of dmenu from the source
-git clone "$dmenu" ".config/suckless"
+git clone "$dmenu"
 cd dmenu
 sudo make clean install
+cd ..
 
-echo "Finished installing suckless software"
+# Clone the lates build of slstatus from the source
+git clone "$slstatus"
+cd slstatus
+sudo make clean install
+cd
+
+echo "Finished installing desktop enviornment"
+
+echo "Finalizing config"
+
+startup="#!/bin/bash
+nitrogen --set-zoom-fill ~/Pictures/Wallpapers/computer.png &
+xinput set-prop 12 321 1 &"
+
+xinit="#!/bin/bash
+# ~/.xinitrc
+# Executed by window manager (run your window manaager from here
+
+exec dwm &
+source ~/.config/startup.sh &
+slstatus"
+
+zshrc="autoload -U colors && colors
+PS1="%B%{$fg[red]%}[%{$fg[yellow]%}%n%{$fg[green]%}@%{$fg[blue]%}%M %{$fg[magenta]%}%~%{$fg[red]%}]%{$reset_color%}$%b "
+HISTFILE=~/.config/zsh/.histfile
+HISTSIZE=1000
+SAVEHIST=1000
+
+unsetopt beep
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+ 
+source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+ 
+# Vi keybindings
+set -o vi"
+
+echo "$startup" > $HOME/.config/startup.sh
+
+echo "$xinit" > .xinitrc
+
+echo "$zshrc" > .zshrc
+
+echo "Config has been finalized."
